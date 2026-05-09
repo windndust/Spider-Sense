425b6a272138bafb701791d0b70a7007f75b8d3f
Michael Neidinger <michael.neidinger@gmail.com>
2026-05-08 22:28:01 -0500

    add logLocation arg to specify log location for logging module

--- basic_python_flask_web_app.py
+++ basic_python_flask_web_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import argparse
+import logging
 
 # -- CONFIGURATION --
 LISTEN_PORT = 8080
@@ -7,9 +8,25 @@ LISTEN_PORT = 8080
 # --- INITIALIZATION ---
 app = Flask(__name__)
 
+def setupLog(logDirectory):
+    if logDirectory.endswith("/"):
+        logLocation=logDirectory+'spider-sense.log'
+    else:
+        logLocation=logDirectory+'/spider-sense.log'
+    print(f"Log location: {logLocation}")
+    logging.basicConfig(
+        filename=logLocation,
+        level=logging.INFO,
+        format='%(asctime)s %(levelname)s %(message)s'
+    )
+    logging.info("********************************")
+    logging.info("************  Start ************")
+    logging.info("********************************")
+
 
 @app.route('/health', methods=['GET'])
 def health_check():
+    logging.info("Health checked")
     return jsonify({"status": "ok", "service": "Flask listener"}), 200
     
 
@@ -17,7 +34,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='A test basic python flask server')
     parser.add_argument('--port', type=int, default=LISTEN_PORT, 
         help=f'Set the port number (default: {LISTEN_PORT} )')
+    parser.add_argument('--logLocation', type=str, default="./",
+        help=f'Set the location for log files', required=True)
     args = parser.parse_args()
 
     print(f"Starting test basic python flask server on port {args.port}")
+    setupLog(args.logLocation)
     app.run(host='0.0.0.0', port=args.port)
