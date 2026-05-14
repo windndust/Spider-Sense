619047a1d253918cc5ae826cb6264d7aafe784e9
Michael Neidinger <michael.neidinger@gmail.com>
2026-05-14 13:54:25 -0500

    added /deployment endpoint, which uses subprocess lib to call local shell scripts

--- basic_python_flask_web_app.py
+++ basic_python_flask_web_app.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, jsonify
 import argparse
 import logging
+import subprocess
+import time
 
 # -- CONFIGURATION --
 LISTEN_PORT = 8080
+DEPLOY_SCRIPTS = "/"
 
 # --- INITIALIZATION ---
 app = Flask(__name__)
@@ -23,12 +26,46 @@ def setupLog(logDirectory):
     logging.info("************  Start ************")
     logging.info("********************************")
 
+@app.route('/deploy', methods=['POST'])
+def deploy():
+    logging.info("Deploy request")
+
+    start_time = time.time()
+    try:
+        
+        result = subprocess.run(
+            ["bash" , DEPLOY_SCRIPTS+"deploy.bash"],
+            shell=True,
+            capture_output=True,
+            text=True,
+            check=True
+        )
+        logging.info(f"Return Code: {result.returncode}, Command: {result.args}")
+        logging.info(f"Script stdout: {result.stdout.strip()}")
+        logging.info(f"Script stderr: {result.stderr.strip()}")
+        return jsonify({"status": "ok", "message": "Deploy Success"}), 200
+
+    except subprocess.CalledProcessError as e:
+        logging.error(f"Deployment failed with return code {e.returncode}")
+        logging.error(f"Command: {e.cmd}, Args: {e.args}")
+        logging.error(f"Standard Error : {e.stderr}")
+        logging.error(f"Standard Output: {e.stdout}")
+        logging.error(f"Output: {e.output}")
+        return jsonify({"status": "error", "service": "Deploy"}), 500
+
+    except Exception as e:
+        logging.exception("Unexpected exception during /deploy")
+        return jsonify({"status": "error", "service": "Deploy"}), 500
+        
+    finally:
+        duration = time.time() - start_time
+        logging.info(f"Duration: {duration:.2f}s")
+
 
 @app.route('/health', methods=['GET'])
 def health_check():
     logging.info("Health checked")
     return jsonify({"status": "ok", "service": "Flask listener"}), 200
-    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='A test basic python flask server')
@@ -36,8 +73,11 @@ if __name__ == '__main__':
         help=f'Set the port number (default: {LISTEN_PORT} )')
     parser.add_argument('--logLocation', type=str, default="./",
         help=f'Set the location for log files', required=True)
+    parser.add_argument('--deployLocation', type=str, 
+        help=f'Set the location for deploy screipts', required=True)
     args = parser.parse_args()
 
     print(f"Starting test basic python flask server on port {args.port}")
     setupLog(args.logLocation)
+    DEPLOY_SCRIPTS = args.deployLocation
     app.run(host='0.0.0.0', port=args.port)
